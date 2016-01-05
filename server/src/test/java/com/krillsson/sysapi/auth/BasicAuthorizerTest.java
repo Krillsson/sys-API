81fb967d1d9075992796c5f13a0cc05274a7ce78
Krillsson <contact@christian-jensen.se>
2016-01-05 17:59:42 +0100

    Add trivial unit tests for BasicAuthorizer

--- server/src/test/java/com/krillsson/sysapi/auth/BasicAuthorizerTest.java
+++ server/src/test/java/com/krillsson/sysapi/auth/BasicAuthorizerTest.java
@@ -1,7 +1,39 @@
 package com.krillsson.sysapi.auth;
 
-import junit.framework.TestCase;
 
-public class BasicAuthorizerTest extends TestCase {
+import com.krillsson.sysapi.UserConfiguration;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BasicAuthorizerTest {
+
+    private final String correctUser = "user";
+    private UserConfiguration userConfiguration;
+
+    private BasicAuthorizer authorizer;
+
+    @Before
+    public void setUp() throws Exception {
+        userConfiguration = new UserConfiguration(correctUser, "");
+        authorizer = new BasicAuthorizer(userConfiguration);
+    }
+
+    @Test
+    public void correctRoleAndName() throws Exception {
+        assertTrue(authorizer.authorize(new UserConfiguration(correctUser, ""), BasicAuthorizer.AUTHENTICATED_ROLE));
+    }
+
+    @Test
+    public void incorrectRole() throws Exception {
+        assertFalse(authorizer.authorize(new UserConfiguration(correctUser, ""), "derp"));
+    }
+
+    @Test
+    public void incorrectUser() throws Exception {
+        assertFalse(authorizer.authorize(new UserConfiguration("derp", ""), BasicAuthorizer.AUTHENTICATED_ROLE));
+    }
 }
