3188a27666078f624e54ebd6134714606cb618b7
Christian <contact@christian-jensen.se>
2017-01-15 19:52:37 +0100

    Add license header to source files

--- domain/src/main/java/com/krillsson/sysapi/domain/health/SensorsData.java
+++ domain/src/main/java/com/krillsson/sysapi/domain/health/SensorsData.java
@@ -1,22 +1,42 @@
+/*
+ * Sys-Api (https://github.com/Krillsson/sys-api)
+ *
+ * Copyright 2017 Christian Jensen / Krillsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Maintainers:
+ * contact[at]christian-jensen[dot]se
+ */
 package com.krillsson.sysapi.domain.health;
 
 import com.krillsson.sysapi.domain.cpu.CpuHealth;
 import com.krillsson.sysapi.domain.gpu.GpuHealth;
 
-import java.util.List;
+import java.util.Map;
 
 public class SensorsData {
     private final CpuHealth cpuHealth;
-    private final GpuHealth[] gpuHealths;
-    private final List<HealthData> healthDatas;
+    private final Map<String, GpuHealth> gpuHealths;
+    private final HealthData[] healthDatas;
 
-    public SensorsData(CpuHealth cpuHealth, GpuHealth[] gpuHealths, List<HealthData> healthDatas) {
+    public SensorsData(CpuHealth cpuHealth, Map<String, GpuHealth> gpuHealths, HealthData[] healthDatas) {
         this.cpuHealth = cpuHealth;
         this.gpuHealths = gpuHealths;
         this.healthDatas = healthDatas;
     }
 
-    public GpuHealth[] getGpuHealths() {
+    public Map<String, GpuHealth> getGpuHealths() {
         return gpuHealths;
     }
 
@@ -24,7 +44,7 @@ public class SensorsData {
         return cpuHealth;
     }
 
-    public List<HealthData> getHealthDatas() {
+    public HealthData[] getHealthDatas() {
         return healthDatas;
     }
 }
