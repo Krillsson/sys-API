3f228f42fa460434baccb445bb402a4dffd2be0f
Christian <contact@christian-jensen.se>
2018-03-29 14:33:34 +0200

    Util class for nullable streams

---
+++ server/src/main/java/com/krillsson/sysapi/util/Streams.java
@@ -0,0 +1,15 @@
+package com.krillsson.sysapi.util;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class Streams<T> {
+    @SuppressWarnings("unchecked")
+    public static <T> java.util.stream.Stream<T> ofNullable(Collection<? extends T> coll) {
+        return coll == null ? java.util.stream.Stream.empty() : (java.util.stream.Stream<T>) coll.stream();
+    }
+
+    public static <T> java.util.stream.Stream<T> ofNullable(T[] arr) {
+        return arr == null || arr.length > 0 ?  java.util.stream.Stream.empty() : Stream.of(arr);
+    }
+}
