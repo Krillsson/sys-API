956a7b44f29ca9cdb3a0bf5b8dbe6073f395f5b4
Christian <contact@christian-jensen.se>
2018-07-08 23:39:44 +0200

    Rename TimeMachine -> Clock

--- server/src/main/java/com/krillsson/sysapi/core/history/History.java
+++ server/src/main/java/com/krillsson/sysapi/core/history/History.java
@@ -1,7 +1,7 @@
 package com.krillsson.sysapi.core.history;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.krillsson.sysapi.util.TimeMachine;
+import com.krillsson.sysapi.util.Clock;
 import org.slf4j.Logger;
 
 import java.time.LocalDateTime;
@@ -12,15 +12,15 @@ public class History<T> {
     private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(History.class);
 
     private final List<HistoryEntry<T>> history;
-    private final TimeMachine timeMachine;
+    private final Clock clock;
 
     protected History() {
-        this(new TimeMachine());
+        this(new Clock());
     }
 
     @VisibleForTesting
-    protected History(TimeMachine timeMachine) {
-        this.timeMachine = timeMachine;
+    protected History(Clock clock) {
+        this.clock = clock;
         this.history = new ArrayList<>();
     }
 
@@ -30,11 +30,11 @@ public class History<T> {
 
     public void record(T value) {
         LOGGER.trace("Recording history for {}", value.getClass().getSimpleName());
-        history.add(new HistoryEntry<>(timeMachine.now(/* with system timezone */), value));
+        history.add(new HistoryEntry<>(clock.now(/* with system timezone */), value));
     }
 
     public void purge(int olderThan, ChronoUnit unit) {
-        LocalDateTime maxAge = timeMachine.now().minus(olderThan, unit);
+        LocalDateTime maxAge = clock.now().minus(olderThan, unit);
         Set<HistoryEntry> toBeRemoved = new HashSet<>();
         for (HistoryEntry historyEntry : history) {
             if (historyEntry.date.isBefore(maxAge)) {
