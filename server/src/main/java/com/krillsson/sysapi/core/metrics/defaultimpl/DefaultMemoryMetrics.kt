ac7b8a3e99fb5a9efe05990889284eb1875462af
Christian Jensen <christian.jensen@m.co>
2020-11-15 12:15:59 +0100

    Migrade DefaultMemoryMetrics to kotlin

--- server/src/main/java/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultMemoryMetrics.kt
+++ server/src/main/java/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultMemoryMetrics.kt
@@ -1,33 +1,32 @@
-package com.krillsson.sysapi.core.metrics.defaultimpl;
+package com.krillsson.sysapi.core.metrics.defaultimpl
 
-import com.krillsson.sysapi.core.domain.memory.MemoryLoad;
-import com.krillsson.sysapi.core.metrics.MemoryMetrics;
-import oshi.hardware.GlobalMemory;
-import oshi.hardware.HardwareAbstractionLayer;
-import oshi.hardware.VirtualMemory;
-import oshi.software.os.OperatingSystem;
+import com.krillsson.sysapi.core.domain.memory.MemoryLoad
+import com.krillsson.sysapi.core.metrics.MemoryMetrics
+import oshi.hardware.GlobalMemory
+import oshi.hardware.HardwareAbstractionLayer
+import oshi.software.os.OperatingSystem
 
-public class DefaultMemoryMetrics implements MemoryMetrics {
-    private final HardwareAbstractionLayer hal;
-    private final OperatingSystem operatingSystem;
-
-    public DefaultMemoryMetrics(HardwareAbstractionLayer hal, OperatingSystem operatingSystem) {
-        this.hal = hal;
-        this.operatingSystem = operatingSystem;
+class DefaultMemoryMetrics(
+    private val hal: HardwareAbstractionLayer,
+    private val operatingSystem: OperatingSystem
+) : MemoryMetrics {
+    override fun memoryLoad(): MemoryLoad {
+        val memory = hal.memory
+        val virtualMemory = memory.virtualMemory
+        return MemoryLoad(
+            operatingSystem.processCount,
+            virtualMemory.swapTotal,
+            virtualMemory.swapUsed,
+            memory.total,
+            memory.available,
+            usedPercent(memory).toDouble()
+        )
     }
 
-    @Override
-    public MemoryLoad memoryLoad() {
-        GlobalMemory memory = hal.getMemory();
-        VirtualMemory virtualMemory = memory.getVirtualMemory();
-        return new MemoryLoad(operatingSystem.getProcessCount(), virtualMemory.getSwapTotal(), virtualMemory.getSwapUsed(), memory.getTotal(), memory.getAvailable(), usedPercent(memory));
+    private fun usedPercent(memory: GlobalMemory): Int {
+        val free = memory.available
+        val total = memory.total
+        val used = total - free
+        return (used * 100.0 / total + 0.5).toInt()
     }
-
-    private int usedPercent(GlobalMemory memory) {
-        long free = memory.getAvailable();
-        long total = memory.getTotal();
-        long used = total - free;
-        return (int) (used * 100.0 / (total) + 0.5);
-    }
-
 }
