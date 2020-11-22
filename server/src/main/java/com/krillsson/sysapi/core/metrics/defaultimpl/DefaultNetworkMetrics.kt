6ece8869655ef4d207e6de8e42fa6b19a57ada89
Christian Jensen <christian.jensen@m.co>
2020-11-22 16:33:50 +0100

    Convert DefaultNetworkMetrics to kotlin

--- server/src/main/java/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultNetworkMetrics.kt
+++ server/src/main/java/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultNetworkMetrics.kt
@@ -18,143 +18,163 @@
  * Maintainers:
  * contact[at]christian-jensen[dot]se
  */
-package com.krillsson.sysapi.core.metrics.defaultimpl;
+package com.krillsson.sysapi.core.metrics.defaultimpl
 
-import com.krillsson.sysapi.core.domain.network.NetworkInterface;
-import com.krillsson.sysapi.core.domain.network.NetworkInterfaceLoad;
-import com.krillsson.sysapi.core.domain.network.NetworkInterfaceSpeed;
-import com.krillsson.sysapi.core.domain.network.NetworkInterfaceValues;
-import com.krillsson.sysapi.core.metrics.NetworkMetrics;
-import com.krillsson.sysapi.core.speed.SpeedMeasurementManager;
-import org.slf4j.Logger;
-import oshi.hardware.HardwareAbstractionLayer;
-import oshi.hardware.NetworkIF;
+import com.krillsson.sysapi.core.domain.network.NetworkInterface
+import com.krillsson.sysapi.core.domain.network.NetworkInterfaceLoad
+import com.krillsson.sysapi.core.domain.network.NetworkInterfaceSpeed
+import com.krillsson.sysapi.core.domain.network.NetworkInterfaceValues
+import com.krillsson.sysapi.core.metrics.NetworkMetrics
+import com.krillsson.sysapi.core.speed.SpeedMeasurementManager
+import com.krillsson.sysapi.core.speed.SpeedMeasurementManager.CurrentSpeed
+import com.krillsson.sysapi.core.speed.SpeedMeasurementManager.SpeedSource
+import org.slf4j.LoggerFactory
+import oshi.hardware.HardwareAbstractionLayer
+import oshi.hardware.NetworkIF
+import java.net.SocketException
+import java.util.Optional
 
-import java.net.SocketException;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+open class DefaultNetworkMetrics(
+    private val hal: HardwareAbstractionLayer,
+    private val speedMeasurementManager: SpeedMeasurementManager
+) : NetworkMetrics {
 
-public class DefaultNetworkMetrics implements NetworkMetrics {
+    class NetworkSpeedSource(private val networkIF: NetworkIF) : SpeedSource {
+        override fun getName(): String {
+            return networkIF.name
+        }
 
-    protected static final NetworkInterfaceSpeed EMPTY_INTERFACE_SPEED = new NetworkInterfaceSpeed(0, 0);
-    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(DefaultNetworkMetrics.class);
-    private static final int BYTE_TO_BIT = 8;
-    private final HardwareAbstractionLayer hal;
-    private final SpeedMeasurementManager speedMeasurementManager;
+        override fun getCurrentRead(): Long {
+            networkIF.updateAttributes()
+            return networkIF.bytesRecv
+        }
 
-    public DefaultNetworkMetrics(HardwareAbstractionLayer hal, SpeedMeasurementManager speedMeasurementManager) {
-        this.hal = hal;
-        this.speedMeasurementManager = speedMeasurementManager;
+        override fun getCurrentWrite(): Long {
+            networkIF.updateAttributes()
+            return networkIF.bytesSent
+        }
     }
 
-    void register() {
-        List<SpeedMeasurementManager.SpeedSource> collect = hal.getNetworkIFs()
-                .stream()
-                .map(n -> new SpeedMeasurementManager.SpeedSource() {
-                    @Override
-                    public String getName() {
-                        return n.getName();
-                    }
-
-                    @Override
-                    public long getCurrentRead() {
-                        n.updateAttributes();
-                        return n.getBytesRecv();
-                    }
-
-                    @Override
-                    public long getCurrentWrite() {
-                        //TODO: maybe it's good enough to do this in getCurrentRead since getCurrentWrite is called immediately after
-                        n.updateAttributes();
-                        return n.getBytesSent();
-                    }
-                })
-                .collect(Collectors.toList());
-        speedMeasurementManager.register(collect);
-    }
-
-    @Override
-    public List<NetworkInterface> networkInterfaces() {
-        return hal.getNetworkIFs().stream().map(mapToNetworkInterface()).collect(Collectors.toList());
-    }
-
-    @Override
-    public Optional<NetworkInterface> networkInterfaceById(String id) {
-        return hal.getNetworkIFs().stream().filter(n -> n.getName().equalsIgnoreCase(id)).map(
-                mapToNetworkInterface()).findAny();
-    }
-
-    @Override
-    public List<NetworkInterfaceLoad> networkInterfaceLoads() {
-        return hal.getNetworkIFs().stream()
-                .map(mapToLoad())
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public Optional<NetworkInterfaceLoad> networkInterfaceLoadById(String id) {
-        return hal.getNetworkIFs().stream()
-                .filter(n -> n.getName().equalsIgnoreCase(id))
-                .map(mapToLoad())
-                .findAny();
-    }
-
-
-    protected NetworkInterfaceSpeed speedForInterfaceWithName(String name) {
-        Optional<SpeedMeasurementManager.CurrentSpeed> currentSpeedForName = speedMeasurementManager.getCurrentSpeedForName(
-                name);
-        return currentSpeedForName.map(s -> new NetworkInterfaceSpeed(
-                s.getReadPerSeconds(),
-                s.getWritePerSeconds()
-        )).orElse(EMPTY_INTERFACE_SPEED);
-    }
-
-
-    Function<NetworkIF, NetworkInterface> mapToNetworkInterface() {
-        return nic -> {
-            boolean loopback = false;
-            try {
-                loopback = nic.queryNetworkInterface().isLoopback();
-            } catch (SocketException e) {
-                //ignore
-                LOGGER.warn("Socket exception while queering for loopback parameter", e);
+    fun register() {
+        speedMeasurementManager.register(
+            hal.networkIFs.map {
+                NetworkSpeedSource(it)
             }
-            return new NetworkInterface(
-                    nic.getName(),
-                    nic.getDisplayName(),
-                    nic.getMacaddr(),
-                    nic.getSpeed(), nic.getMTU(),
-                    loopback,
-                    Stream.of(nic.getIPv4addr()).collect(Collectors.toList()),
-                    Stream.of(nic.getIPv6addr()).collect(Collectors.toList())
-            );
-        };
+        )
     }
 
-    private Function<NetworkIF, NetworkInterfaceLoad> mapToLoad() {
-        return n -> {
-            boolean up = false;
+    override fun networkInterfaces(): List<NetworkInterface> {
+
+        return hal.networkIFs
+            .map {
+                var isLoopback = false
+                try {
+                    isLoopback = it.queryNetworkInterface().isLoopback
+                } catch (e: SocketException) {
+                    //ignore
+                    LOGGER.warn("Socket exception while queering for loopback parameter", e)
+                }
+                it.asNetworkInterface(isLoopback)
+            }.toList()
+    }
+
+    override fun networkInterfaceById(id: String): Optional<NetworkInterface> {
+        return Optional.ofNullable(hal.networkIFs.firstOrNull {
+            it.name.equals(
+                id,
+                ignoreCase = true
+            )
+        }
+            ?.let {
+                var isLoopback = false
+                try {
+                    isLoopback = it.queryNetworkInterface().isLoopback
+                } catch (e: SocketException) {
+                    //ignore
+                    LOGGER.warn("Socket exception while queering for loopback parameter", e)
+                }
+                it.asNetworkInterface(isLoopback)
+            })
+    }
+
+    override fun networkInterfaceLoads(): List<NetworkInterfaceLoad> {
+        return hal.networkIFs.map {
+            var up = false
             try {
-                up = n.queryNetworkInterface().isUp();
-            } catch (SocketException e) {
-                LOGGER.error("Error occurred while getting status for NIC", e);
+                up = it.queryNetworkInterface().isUp
+            } catch (e: SocketException) {
+                LOGGER.error("Error occurred while getting status for NIC", e)
             }
-            return new NetworkInterfaceLoad(
-                    n.getName(), up,
-                    new NetworkInterfaceValues(
-                            n.getSpeed(),
-                            n.getBytesRecv(),
-                            n.getBytesSent(),
-                            n.getPacketsRecv(),
-                            n.getPacketsSent(),
-                            n.getInErrors(),
-                            n.getOutErrors()
-                    ),
-                    speedForInterfaceWithName(n.getName())
-            );
-        };
+            it.asNetworkInterfaceLoad(up, speedForInterfaceWithName(it.name))
+        }.toList()
+    }
+
+    override fun networkInterfaceLoadById(id: String): Optional<NetworkInterfaceLoad> {
+        return Optional.ofNullable(hal.networkIFs.firstOrNull {
+            it.name.equals(
+                id,
+                ignoreCase = true
+            )
+        }
+            ?.let {
+                var up = false
+                try {
+                    up = it.queryNetworkInterface().isUp
+                } catch (e: SocketException) {
+                    LOGGER.error("Error occurred while getting status for NIC", e)
+                }
+                it.asNetworkInterfaceLoad(up, speedForInterfaceWithName(it.name))
+            })
+    }
+
+    protected open fun speedForInterfaceWithName(name: String?): NetworkInterfaceSpeed {
+        val currentSpeedForName = speedMeasurementManager.getCurrentSpeedForName(
+            name
+        )
+        return currentSpeedForName.map { s: CurrentSpeed ->
+            NetworkInterfaceSpeed(
+                s.readPerSeconds,
+                s.writePerSeconds
+            )
+        }.orElse(EMPTY_INTERFACE_SPEED)
+    }
+
+    private fun NetworkIF.asNetworkInterface(isLoopback: Boolean): NetworkInterface =
+        NetworkInterface(
+            name,
+            displayName,
+            macaddr,
+            speed,
+            mtu,
+            isLoopback,
+            iPv4addr.asList(),
+            iPv6addr.asList()
+        )
+
+    private fun NetworkIF.asNetworkInterfaceLoad(
+        up: Boolean,
+        nicSpeed: NetworkInterfaceSpeed
+    ): NetworkInterfaceLoad = NetworkInterfaceLoad(
+        name,
+        up,
+        NetworkInterfaceValues(
+            speed,
+            bytesRecv,
+            bytesSent,
+            packetsRecv,
+            packetsSent,
+            inErrors,
+            outErrors
+        ),
+        nicSpeed
+    )
+
+    companion object {
+        @JvmField
+        protected val EMPTY_INTERFACE_SPEED = NetworkInterfaceSpeed(0, 0)
+        private val LOGGER = LoggerFactory.getLogger(
+            DefaultNetworkMetrics::class.java
+        )
+        private const val BYTE_TO_BIT = 8
     }
 }
