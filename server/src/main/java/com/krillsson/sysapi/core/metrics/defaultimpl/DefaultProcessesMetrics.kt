65bed21cff18ba1ad43ee83cf6691bc5bc251f5c
Christian Jensen <christian.jensen@m.co>
2020-11-22 16:40:46 +0100

    Convert DefaultProcessesMetrics to kotlin

--- server/src/main/java/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultProcessesMetrics.kt
+++ server/src/main/java/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultProcessesMetrics.kt
@@ -1,79 +1,75 @@
-package com.krillsson.sysapi.core.metrics.defaultimpl;
+package com.krillsson.sysapi.core.metrics.defaultimpl
 
-import com.krillsson.sysapi.core.domain.memory.MemoryLoad;
-import com.krillsson.sysapi.core.domain.processes.Process;
-import com.krillsson.sysapi.core.domain.processes.ProcessSort;
-import com.krillsson.sysapi.core.domain.processes.ProcessesInfo;
-import com.krillsson.sysapi.core.metrics.ProcessesMetrics;
-import com.krillsson.sysapi.util.ExtensionsKt;
-import org.jetbrains.annotations.NotNull;
-import oshi.hardware.GlobalMemory;
-import oshi.hardware.HardwareAbstractionLayer;
-import oshi.hardware.VirtualMemory;
-import oshi.software.os.OSProcess;
-import oshi.software.os.OperatingSystem;
+import com.krillsson.sysapi.core.domain.processes.Process
+import com.krillsson.sysapi.core.domain.processes.ProcessSort
+import com.krillsson.sysapi.core.domain.processes.ProcessesInfo
+import com.krillsson.sysapi.core.metrics.ProcessesMetrics
+import com.krillsson.sysapi.util.asOshiProcessSort
+import oshi.hardware.GlobalMemory
+import oshi.hardware.HardwareAbstractionLayer
+import oshi.software.os.OSProcess
+import oshi.software.os.OperatingSystem
+import java.util.Optional
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-public class DefaultProcessesMetrics implements ProcessesMetrics {
-    private final OperatingSystem operatingSystem;
-    private final HardwareAbstractionLayer hal;
-
-    public DefaultProcessesMetrics(OperatingSystem operatingSystem, HardwareAbstractionLayer hal) {
-        this.operatingSystem = operatingSystem;
-        this.hal = hal;
-    }
-
-    @Override
-    public Optional<Process> getProcessByPid(int pid) {
-        MemoryLoad memory = memoryLoad();
+class DefaultProcessesMetrics(
+    private val operatingSystem: OperatingSystem,
+    private val hal: HardwareAbstractionLayer
+) : ProcessesMetrics {
+    override fun getProcessByPid(pid: Int): Optional<Process> {
         return Optional
-                .of(operatingSystem.getProcess(pid))
-                .map((OSProcess p) -> Process.create(p, memory));
+            .of(operatingSystem.getProcess(pid))
+            .map { process: OSProcess ->
+                process.asProcess(hal.memory.total)
+            }
     }
 
-    public MemoryLoad memoryLoad() {
-        GlobalMemory memory = hal.getMemory();
-        VirtualMemory virtualMemory = memory.getVirtualMemory();
-        return new MemoryLoad(
-                operatingSystem.getProcessCount(),
-                virtualMemory.getSwapTotal(),
-                virtualMemory.getSwapUsed(),
-                memory.getTotal(),
-                memory.getAvailable(),
-                usedPercent(memory)
-        );
-    }
-
-    @NotNull
-    @Override
-    public ProcessesInfo processesInfo(@NotNull ProcessSort sortBy, int limit) {
-        MemoryLoad memory = memoryLoad();
-        List<Process> processes = Collections.emptyList();
+    override fun processesInfo(sortBy: ProcessSort, limit: Int): ProcessesInfo {
+        val totalBytes = hal.memory.total
+        var processes: List<Process> = emptyList()
         if (limit > -1) {
-            processes = operatingSystem.getProcesses(limit, ExtensionsKt.asOshiProcessSort(sortBy))
-                    .stream()
-                    .map(p -> Process.create(p, memory))
-                    .collect(Collectors.toList());
+            processes = operatingSystem.getProcesses(limit, sortBy.asOshiProcessSort())
+                .map { process: OSProcess ->
+                    process.asProcess(totalBytes)
+                }
         }
-        return new ProcessesInfo(
-                memory,
-                operatingSystem.getProcessId(),
-                operatingSystem.getThreadCount(),
-                operatingSystem.getProcessCount(),
-                processes
-        );
+        return ProcessesInfo(
+            operatingSystem.processId.toLong(),
+            operatingSystem.threadCount.toLong(),
+            operatingSystem.processCount.toLong(),
+            processes
+        )
     }
 
-    private int usedPercent(GlobalMemory memory) {
-        long free = memory.getAvailable();
-        long total = memory.getTotal();
-        long used = total - free;
-        return (int) (used * 100.0 / (total) + 0.5);
+    private fun OSProcess.asProcess(totalBytes: Long): Process {
+        return Process(
+            name,
+            path,
+            commandLine,
+            user,
+            userID,
+            group,
+            groupID,
+            state,
+            processID,
+            parentProcessID,
+            threadCount,
+            priority,
+            virtualSize,
+            residentSetSize,
+            100.0 * residentSetSize / totalBytes,
+            kernelTime, userTime,
+            upTime,
+            100.0 * (kernelTime + userTime) / upTime,
+            startTime,
+            bytesRead,
+            bytesWritten
+        )
+    }
+
+    private fun usedPercent(memory: GlobalMemory): Int {
+        val free = memory.available
+        val total = memory.total
+        val used = total - free
+        return (used * 100.0 / total + 0.5).toInt()
     }
 }
-
