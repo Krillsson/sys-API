232472bfdf002c8915a15b6cce51ee9e80ad44ee
Christian Jensen <christian.jensen@m.co>
2022-03-05 00:20:39 +0100

    Fixes issue with adding numerical monitors

--- server/src/main/java/com/krillsson/sysapi/graphql/scalars/LongCoercing.kt
+++ server/src/main/java/com/krillsson/sysapi/graphql/scalars/LongCoercing.kt
@@ -1,22 +1,40 @@
 package com.krillsson.sysapi.graphql.scalars
 
+import graphql.language.IntValue
 import graphql.language.StringValue
 import graphql.schema.Coercing
+import graphql.schema.CoercingParseValueException
+import graphql.schema.CoercingSerializeException
 
-class LongCoercing : Coercing<Any?, String> {
-    override fun serialize(input: Any): String {
-        return (input as Long).toString()
+class LongCoercing : Coercing<Long?, String?> {
+    override fun serialize(dataFetcherResult: Any): String? {
+        return try {
+            val value = dataFetcherResult as Long
+            value.toString()
+        } catch (e: Exception) {
+            throw CoercingSerializeException(e)
+        }
     }
 
-    override fun parseValue(input: Any): Any? {
-        return serialize(input)
+    override fun parseValue(input: Any): Long? {
+        return parse(input)
     }
 
     override fun parseLiteral(input: Any): Long? {
-        return when (input) {
-            is StringValue -> (input as Long).toLong()
-            is Long -> input.toLong()
-            else -> null
+        return parse(input)
+    }
+
+    private fun parse(input: Any): Long? {
+        return try {
+            when (input) {
+                is String -> input.toLong()
+                is StringValue -> input.value.toLong()
+                is IntValue -> input.value.toLong()
+                is Int -> input.toLong()
+                else -> throw CoercingParseValueException("$input is not any known type")
+            }
+        } catch (e: Exception) {
+            throw CoercingParseValueException(e)
         }
     }
 }
