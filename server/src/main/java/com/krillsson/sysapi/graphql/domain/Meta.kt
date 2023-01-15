457df51a4859db41bebf5cf96b9fe3da71f4a4a8
Christian Jensen <christian.jensen@m.co>
2023-01-15 10:52:11 +0100

    #37 add endpoints to GraphQL

--- server/src/main/java/com/krillsson/sysapi/graphql/domain/Meta.kt
+++ server/src/main/java/com/krillsson/sysapi/graphql/domain/Meta.kt
@@ -3,5 +3,6 @@ package com.krillsson.sysapi.graphql.domain
 data class Meta(
     val version: String,
     val buildDate: String,
-    val processId: Int
+    val processId: Int,
+    val endpoints: List<String>
 )
