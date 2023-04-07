7ed3b5e9ecde056dc4a990e6210cc329f63a7549
Christian Jensen <christian.jensen@m.co>
2023-04-07 10:45:50 +0200

    #85 retry evicted Tickers after a while

--- server/src/main/java/com/krillsson/sysapi/util/Ticker.kt
+++ server/src/main/java/com/krillsson/sysapi/util/Ticker.kt
@@ -2,11 +2,16 @@ package com.krillsson.sysapi.util
 
 import io.dropwizard.lifecycle.Managed
 import java.time.Duration
+import java.time.Instant
 import java.util.*
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
-class Ticker(private val executorService: ScheduledExecutorService, measurementInterval: Int) : Managed {
+class Ticker(
+    private val executorService: ScheduledExecutorService,
+    private val measurementIntervalSeconds: Long,
+    private val retryIntervalSeconds: Long
+) : Managed {
     interface TickListener {
         fun onTick()
     }
@@ -23,13 +28,14 @@ class Ticker(private val executorService: ScheduledExecutorService, measurementI
         data class Evicted(
             override val tickListener: TickListener,
             override val id: UUID,
+            val evictedAt: Instant,
             val error: Throwable
         ) : TickContainer
     }
 
     private val logger by logger()
-    private val measurementInterval: Long = Duration.ofSeconds(measurementInterval.toLong()).seconds
-    private val containers: MutableList<TickContainer> = ArrayList()
+    private val containers: MutableList<TickContainer.Active> = mutableListOf()
+    private val evictedContainers: MutableList<TickContainer.Evicted> = mutableListOf()
 
 
     fun register(listener: TickListener) {
@@ -42,28 +48,45 @@ class Ticker(private val executorService: ScheduledExecutorService, measurementI
 
     @Throws(Exception::class)
     override fun start() {
-        executorService.scheduleAtFixedRate({ execute() }, 1, measurementInterval, TimeUnit.SECONDS)
+        executorService.scheduleAtFixedRate({ execute() }, 1, measurementIntervalSeconds, TimeUnit.SECONDS)
     }
 
     private fun execute() {
+        retryEvicted()
         val evictedContainers = mutableListOf<TickContainer.Evicted>()
         for (listener in containers) {
             try {
-                when (listener) {
-                    is TickContainer.Active -> listener.tickListener.onTick()
-                    is TickContainer.Evicted -> {
-                        logger.warn("Not executing evicted ${listener.tickListener::class.java.simpleName}/${listener.id} due to previous error ${listener.error::class.java.simpleName} ${listener.error.message}")
-                    }
-                }
+                listener.tickListener.onTick()
             } catch (e: Throwable) {
-                logger.error("Error while executing ticker. Evicting...", e)
-                val evicted = TickContainer.Evicted(listener.tickListener, listener.id, e)
+                logger.error(
+                    "Error while executing ticker ${listener.tickListener::class.java.simpleName}/${listener.id} Evicting...",
+                    e
+                )
+                val evicted = TickContainer.Evicted(listener.tickListener, listener.id, Instant.now(), e)
                 evictedContainers += evicted
             }
         }
-        evictedContainers.forEach { evicted ->
+        evict(evictedContainers)
+    }
+
+    private fun retryEvicted() {
+        val evictedToConsider = evictedContainers.toList()
+        val now = Instant.now()
+        evictedToConsider.forEach { evicted ->
+            if (Duration.between(evicted.evictedAt, now).seconds >= retryIntervalSeconds) {
+                logger.info("Retrying ticker ${evicted.tickListener::class.java.simpleName}/${evicted.id} after ${retryIntervalSeconds}s")
+                evictedContainers.removeIf { it.id == evicted.id }
+                containers.add(
+                    TickContainer.Active(evicted.tickListener, evicted.id)
+                )
+            }
+        }
+    }
+
+    private fun evict(containersToEvict: List<TickContainer.Evicted>) {
+        containersToEvict.forEach { evicted ->
             containers.removeIf { it.id == evicted.id }
-            containers.add(evicted)
+            evictedContainers.add(evicted)
         }
     }
 
