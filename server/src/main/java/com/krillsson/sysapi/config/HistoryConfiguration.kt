19f82cc18809f27dacb87b39c509c358497c912b
Christian <christian.jensen@m.co>
2023-05-28 18:18:34 +0200

    #76 update branch based on latest changes

--- server/src/main/java/com/krillsson/sysapi/config/HistoryConfiguration.kt
+++ server/src/main/java/com/krillsson/sysapi/config/HistoryConfiguration.kt
@@ -1,10 +1,7 @@
 package com.krillsson.sysapi.config
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.util.concurrent.TimeUnit
 
 class HistoryConfiguration(
-    @JsonProperty val purging: HistoryPurgingConfiguration,
-    @JsonProperty val interval: Long,
-    @JsonProperty val unit: TimeUnit
+    @JsonProperty val purging: HistoryPurgingConfiguration
 )
--- server/src/main/java/com/krillsson/sysapi/config/UpnpIgdConfiguration.kt
+++ server/src/main/java/com/krillsson/sysapi/config/UpnpIgdConfiguration.kt
@@ -1,6 +1,7 @@
 package com.krillsson.sysapi.config
 
 import com.fasterxml.jackson.annotation.JsonProperty
+
 data class UpnpIgdConfiguration(
     @JsonProperty val enabled: Boolean,
 )
