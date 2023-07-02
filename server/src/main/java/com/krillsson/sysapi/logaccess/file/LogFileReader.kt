d38b16d0cdb377de146944ceef76120f8b3a9b81
Christian <christian.jensen@m.co>
2023-07-02 22:54:28 +0200

    #109 expose log-file creation and modification time in graphql

--- server/src/main/java/com/krillsson/sysapi/logaccess/file/LogFileReader.kt
+++ server/src/main/java/com/krillsson/sysapi/logaccess/file/LogFileReader.kt
@@ -3,9 +3,12 @@ package com.krillsson.sysapi.logaccess.file
 import org.apache.commons.io.input.ReversedLinesFileReader
 import java.io.File
 import java.io.FileInputStream
+import java.io.IOException
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributes
+import java.time.Instant
 
 // log reader that naively reads all lines. May exhaust resources
 class LogFileReader(val file: File) {
@@ -16,7 +19,10 @@ class LogFileReader(val file: File) {
 
     fun path(): String = file.absolutePath
 
+    fun createdAt(): Instant? = readTimeAttribute { it.creationTime().toInstant() }
+    fun updatedAt(): Instant? = readTimeAttribute { it.lastModifiedTime().toInstant() }
     fun sizeBytes() = Files.size(Paths.get(file.path))
+
     fun lines(): List<String> {
         val reader = ReversedLinesFileReader(file, Charset.defaultCharset())
         val lines = mutableListOf<String>()
@@ -26,7 +32,6 @@ class LogFileReader(val file: File) {
         }
         return lines
     }
-
     private fun countLines(file: File): Int {
         var lines = 0
         val fis = FileInputStream(file)
@@ -41,5 +46,15 @@ class LogFileReader(val file: File) {
         }
         fis.close()
         return lines
+    }
+
+    private fun readTimeAttribute(action: (BasicFileAttributes) -> Instant?): Instant? {
+        return try {
+            val attributes: BasicFileAttributes =
+                Files.readAttributes(Paths.get(file.path), BasicFileAttributes::class.java)
+            action(attributes)
+        } catch (ex: IOException) {
+            null
+        }
     }
 }
