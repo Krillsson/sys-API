47b774e1e669f4c2611fb5f3e4dd18e2b77d87fe
Christian <christian.jensen@m.co>
2023-07-16 10:16:38 +0200

    #109 fixes after rebase

--- server/src/main/java/com/krillsson/sysapi/config/MetricsConfiguration.kt
+++ server/src/main/java/com/krillsson/sysapi/config/MetricsConfiguration.kt
@@ -3,7 +3,7 @@ package com.krillsson.sysapi.config
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class MetricsConfiguration(
-        @JsonProperty val monitor: MonitorConfiguration,
+    @JsonProperty val monitor: MonitorConfiguration,
     @JsonProperty val history: HistoryConfiguration,
-        @JsonProperty val cache: CacheConfiguration
+    @JsonProperty val cache: CacheConfiguration
 )
