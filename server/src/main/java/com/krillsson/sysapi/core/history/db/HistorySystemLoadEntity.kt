778e01551c315faa831f96946e28d26d4550ce0f
Christian <christian.jensen@m.co>
2023-07-23 23:43:21 +0200

    #114 fix historyBetweenDates query

--- server/src/main/java/com/krillsson/sysapi/core/history/db/HistorySystemLoadEntity.kt
+++ server/src/main/java/com/krillsson/sysapi/core/history/db/HistorySystemLoadEntity.kt
@@ -6,8 +6,13 @@ import javax.persistence.*
 
 // https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/
 // https://stackoverflow.com/a/50378345
-@MappedSuperclass
-open class BasicHistorySystemLoadEntity(
+@Entity(name = "BasicHistorySystemLoadEntity")
+@Table(name = "HistorySystemLoadEntity")
+@NamedQuery(
+    name = "com.krillsson.sysapi.core.history.db.BasicHistorySystemLoadEntity.findAll",
+    query = "SELECT e FROM BasicHistorySystemLoadEntity e"
+)
+class BasicHistorySystemLoadEntity(
     @Id
     val id: UUID,
     val date: OffsetDateTime,
@@ -15,22 +20,17 @@ open class BasicHistorySystemLoadEntity(
     val systemLoadAverage: Double,
 )
 
-
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @NamedQuery(
     name = "com.krillsson.sysapi.core.history.db.HistorySystemLoadEntity.findAll",
     query = "SELECT e FROM HistorySystemLoadEntity e"
 )
-@NamedQuery(
-    name = "com.krillsson.sysapi.core.history.db.BasicHistorySystemLoadEntity.findAll",
-    query = "SELECT e FROM HistorySystemLoadEntity e"
-)
 class HistorySystemLoadEntity(
-    id: UUID,
-    date: OffsetDateTime,
-    uptime: Long,
-    systemLoadAverage: Double,
+    @Id
+    val id: UUID,
+    val date: OffsetDateTime,
+    val uptime: Long,
+    val systemLoadAverage: Double,
     @OneToOne(cascade = [CascadeType.ALL], orphanRemoval = true)
     val cpuLoad: CpuLoad,
     @OneToMany(mappedBy = "history", cascade = [CascadeType.ALL], orphanRemoval = true)
@@ -49,4 +49,4 @@ class HistorySystemLoadEntity(
     val gpuLoads: List<GpuLoad>,
     @OneToMany(mappedBy = "history", cascade = [CascadeType.ALL], orphanRemoval = true)
     val motherboardHealth: List<HealthData>
-) : BasicHistorySystemLoadEntity(id, date, uptime, systemLoadAverage)
+)
