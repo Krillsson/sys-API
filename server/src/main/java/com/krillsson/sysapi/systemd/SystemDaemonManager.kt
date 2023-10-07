6dd26917b2040cf6381282b04323bde425c16226
Christian <christian.jensen@m.co>
2023-10-07 10:09:29 +0200

    #113 fix "System has not been booted with systemd as init system (PID 1). Can't operate. Failed to connect to bus: Host is down"

--- server/src/main/java/com/krillsson/sysapi/systemd/SystemDaemonManager.kt
+++ server/src/main/java/com/krillsson/sysapi/systemd/SystemDaemonManager.kt
@@ -11,6 +11,12 @@ class SystemDaemonManager(
     private val config: LinuxConfiguration
 ) : SystemDaemonAccessAvailable {
 
+    sealed class Status {
+        object Available : Status()
+        object Disabled : Status()
+        data class Unavailable(val error: RuntimeException) : Status()
+    }
+
     val logger by logger()
 
     private val journalCtl = JournalCtl(mapper, config.journalLogs)
@@ -18,12 +24,21 @@ class SystemDaemonManager(
 
     private val supportedBySystem = journalCtl.supportedBySystem() && systemCtl.supportedBySystem()
 
-    fun supportedBySystem(): Boolean {
-        return supportedBySystem
+    fun status(): Status {
+        return when {
+            !config.systemDaemonServiceManagement.enabled -> Status.Disabled
+            !supportedBySystem() -> Status.Unavailable(RuntimeException("systemctl or journalctl command was not found"))
+            else -> Status.Available
+        }
     }
 
-    override fun openJournal(name: String, limit: Int) =
-        journalCtl.lines(name, limit)
+    override fun openJournal(name: String, limit: Int): List<SystemDaemonJournalEntry> {
+        return if (config.journalLogs.enabled) {
+            journalCtl.lines(name, limit)
+        } else {
+            emptyList()
+        }
+    }
 
     override fun services(): List<SystemCtl.ListServicesOutput.Item> {
         return systemCtl.services()
@@ -35,5 +50,9 @@ class SystemDaemonManager(
 
     fun performCommandWithService(serviceName: String, command: SystemDaemonCommand): CommandResult {
         return systemCtl.performCommandWithService(serviceName, command)
+    }
+
+    private fun supportedBySystem(): Boolean {
+        return supportedBySystem
     }
 }
