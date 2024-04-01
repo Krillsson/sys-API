fbc873af617f3cad701e40871e193b0117a364ba
Christian <christian.jensen@m.co>
2024-04-01 21:30:26 +0200

    #131 update reflect-config.json

--- src/main/kotlin/com/krillsson/sysapi/SysApiApplication.kt
+++ src/main/kotlin/com/krillsson/sysapi/SysApiApplication.kt
@@ -7,7 +7,6 @@ import com.krillsson.sysapi.tls.CertificateNamesCreator
 import com.krillsson.sysapi.tls.SelfSignedCertificateManager
 import com.krillsson.sysapi.util.FileSystem
 import com.krillsson.sysapi.util.logger
-import org.slf4j.LoggerFactory
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -15,33 +14,32 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ImportRuntimeHints
 import oshi.util.GlobalConfig
-import java.io.File
 
 @SpringBootApplication(scanBasePackages = ["com.krillsson.sysapi"])
 @ImportRuntimeHints(RuntimeHint::class)
 @RegisterReflectionForBinding(
-    CacheConfiguration::class,
-    ConnectivityCheckConfiguration::class,
-    DockerConfiguration::class,
-    GraphQLPlayGroundConfiguration::class,
-    HistoryConfiguration::class,
-    HistoryPurgingConfiguration::class,
-    LinuxConfiguration::class,
-    LogReaderConfiguration::class,
-    MdnsConfiguration::class,
-    MetricsConfiguration::class,
-    MonitorConfiguration::class,
-    ProcessesConfiguration::class,
-    SelfSignedCertificateConfiguration::class,
-    UpdateCheckConfiguration::class,
-    UpnpIgdConfiguration::class,
-    UserConfiguration::class,
-    WindowsConfiguration::class,
-    YAMLConfigFile::class,
-    DockerConfigFile::class,
-    AuthConfig::class,
-    com.sun.jna.platform.mac.SystemB.Timeval::class,
-    oshi.jna.ByRef::class,
+        CacheConfiguration::class,
+        ConnectivityCheckConfiguration::class,
+        DockerConfiguration::class,
+        GraphQLPlayGroundConfiguration::class,
+        HistoryConfiguration::class,
+        HistoryPurgingConfiguration::class,
+        LinuxConfiguration::class,
+        LogReaderConfiguration::class,
+        MdnsConfiguration::class,
+        MetricsConfiguration::class,
+        MonitorConfiguration::class,
+        ProcessesConfiguration::class,
+        SelfSignedCertificateConfiguration::class,
+        UpdateCheckConfiguration::class,
+        UpnpIgdConfiguration::class,
+        UserConfiguration::class,
+        WindowsConfiguration::class,
+        YAMLConfigFile::class,
+        DockerConfigFile::class,
+        AuthConfig::class,
+        com.sun.jna.platform.mac.SystemB.Timeval::class,
+        oshi.jna.ByRef::class,
 )
 // https://www.graalvm.org/latest/reference-manual/native-image/dynamic-features/JNI/
 // Failed to parse docker config.json
@@ -51,25 +49,20 @@ class SysAPIApplication {
 
     @Bean
     fun postStartupHook(
-        selfSignedCertificateManager: SelfSignedCertificateManager,
-        certificateNamesCreator: CertificateNamesCreator,
-        config: YAMLConfigFile
+            selfSignedCertificateManager: SelfSignedCertificateManager,
+            certificateNamesCreator: CertificateNamesCreator,
+            config: YAMLConfigFile
     ): ApplicationRunner =
-        ApplicationRunner {
-            GlobalConfig.set("oshi.os.windows.loadaverage", true)
-            if (!FileSystem.data.isDirectory) {
-                logger.info("Attempting to create {}", FileSystem.data)
-                assert(FileSystem.data.mkdir()) { "Unable to create ${FileSystem.data}" }
+            ApplicationRunner {
+                GlobalConfig.set("oshi.os.windows.loadaverage", true)
+                if (!FileSystem.data.isDirectory) {
+                    logger.info("Attempting to create {}", FileSystem.data)
+                    assert(FileSystem.data.mkdir()) { "Unable to create ${FileSystem.data}" }
+                }
+                selfSignedCertificateManager.start(certificateNamesCreator, config.selfSignedCertificates)
             }
-            selfSignedCertificateManager.start(certificateNamesCreator, config.selfSignedCertificates)
-        }
 }
 
 fun main(args: Array<String>) {
-    val logger = LoggerFactory.getLogger(SysAPIApplication::class.java.name.removeSuffix("\$Companion"))
-    val tmpDirEnv = System.getenv("TMPDIR")?.let { File(it) }
-    val tmpDirProp = System.getProperty("java.io.tmpdir")?.let { File(it) }
-    logger.info("${if(tmpDirEnv?.canWrite() == true) "Can write to" else "Unable to write to"} ${tmpDirEnv?.absolutePath ?: "TMPDIR is null"}")
-    logger.info("${if(tmpDirProp?.canWrite() == true) "Can write to" else "Unable to write to"} ${tmpDirProp?.absolutePath} ?: java.io.tmpdir is null")
     runApplication<SysAPIApplication>(*args)
 }
