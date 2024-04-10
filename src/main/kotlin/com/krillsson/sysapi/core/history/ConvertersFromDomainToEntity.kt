96d0c8fcef08e10ddd05ea4ab5fe8796f2fc1287
Christian <christian.jensen@m.co>
2024-04-10 07:02:19 +0200

    #131 fix constraint violation in container statistics

--- src/main/kotlin/com/krillsson/sysapi/core/history/ConvertersFromDomainToEntity.kt
+++ src/main/kotlin/com/krillsson/sysapi/core/history/ConvertersFromDomainToEntity.kt
@@ -13,96 +13,96 @@ fun SystemHistoryEntry.asEntity(): HistorySystemLoadEntity {
 
 fun HistorySystemLoad.asEntity(id: UUID, dateTime: Instant): HistorySystemLoadEntity {
     return HistorySystemLoadEntity(
-        id,
-        dateTime,
-        uptime,
-        systemLoadAverage,
-        cpuLoad.asCpuLoadEntity(id),
-        networkInterfaceLoads.map { it.asNetworkInterfaceLoad(id) },
-        connectivity.asConnectivity(id),
-        diskLoads.map { it.asDiskLoad(id) },
-        fileSystemLoads.map { it.asFileSystemLoad(id) },
-        memory.asMemoryLoad(id),
-        gpuLoads.map { it.asGpuLoad(id) },
-        motherboardHealth.map { it.asMotherboardHealthData(id) }
+            id,
+            dateTime,
+            uptime,
+            systemLoadAverage,
+            cpuLoad.asCpuLoadEntity(id),
+            networkInterfaceLoads.map { it.asNetworkInterfaceLoad(id) },
+            connectivity.asConnectivity(id),
+            diskLoads.map { it.asDiskLoad(id) },
+            fileSystemLoads.map { it.asFileSystemLoad(id) },
+            memory.asMemoryLoad(id),
+            gpuLoads.map { it.asGpuLoad(id) },
+            motherboardHealth.map { it.asMotherboardHealthData(id) }
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.filesystem.FileSystemLoad.asFileSystemLoad(uuid: UUID): FileSystemLoad {
     return FileSystemLoad(
-        UUID.randomUUID(),
-        null,
-        uuid,
-        name,
-        id,
-        freeSpaceBytes,
-        usableSpaceBytes,
-        totalSpaceBytes
+            UUID.randomUUID(),
+            null,
+            uuid,
+            name,
+            id,
+            freeSpaceBytes,
+            usableSpaceBytes,
+            totalSpaceBytes
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.disk.DiskLoad.asDiskLoad(id: UUID): DiskLoad {
     return DiskLoad(
-        UUID.randomUUID(),
-        null,
-        id,
-        name,
-        serial,
-        values.asDiskValues(),
-        speed.asSpeed()
+            UUID.randomUUID(),
+            null,
+            id,
+            name,
+            serial,
+            values.asDiskValues(),
+            speed.asSpeed()
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.disk.DiskSpeed.asSpeed(): DiskSpeed {
     return DiskSpeed(
-        readBytesPerSecond, writeBytesPerSecond
+            readBytesPerSecond, writeBytesPerSecond
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.disk.DiskValues.asDiskValues(): DiskValues {
     return DiskValues(
-        reads, readBytes, writes, writeBytes
+            reads, readBytes, writes, writeBytes
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.sensors.HealthData.asMotherboardHealthData(id: UUID): HealthData {
     return HealthData(
-        UUID.randomUUID(),
-        null,
-        id,
-        description,
-        data,
-        dataType.asDataType()
+            UUID.randomUUID(),
+            null,
+            id,
+            description,
+            data,
+            dataType.asDataType()
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.gpu.GpuLoad.asGpuLoad(id: UUID): GpuLoad {
     return GpuLoad(
-        UUID.randomUUID(),
-        null,
-        id,
-        name,
-        coreLoad,
-        memoryLoad,
-        health.asGpuHealth()
+            UUID.randomUUID(),
+            null,
+            id,
+            name,
+            coreLoad,
+            memoryLoad,
+            health.asGpuHealth()
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.gpu.GpuHealth.asGpuHealth(): GpuHealth {
     return GpuHealth(
-        fanRpm, fanPercent, temperature
+            fanRpm, fanPercent, temperature
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.memory.MemoryLoad.asMemoryLoad(id: UUID): MemoryLoad {
     return MemoryLoad(
-        id,
-        numberOfProcesses,
-        swapTotalBytes,
-        swapUsedBytes,
-        totalBytes,
-        availableBytes,
-        usedPercent
+            id,
+            numberOfProcesses,
+            swapTotalBytes,
+            swapUsedBytes,
+            totalBytes,
+            availableBytes,
+            usedPercent
     )
 }
 
@@ -119,119 +119,120 @@ private fun com.krillsson.sysapi.core.domain.sensors.DataType.asDataType(): Data
 
 private fun com.krillsson.sysapi.core.domain.network.Connectivity.asConnectivity(id: UUID): Connectivity {
     return Connectivity(
-        id,
-        externalIp,
-        previousExternalIp,
-        localIp,
-        connected
+            id,
+            externalIp,
+            previousExternalIp,
+            localIp,
+            connected
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.network.NetworkInterfaceLoad.asNetworkInterfaceLoad(id: UUID): NetworkInterfaceLoad {
     return NetworkInterfaceLoad(
-        UUID.randomUUID(),
-        null,
-        id,
-        name,
-        mac,
-        isUp,
-        values.asNetworkInterfaceValues(),
-        speed.asSpeed(),
+            UUID.randomUUID(),
+            null,
+            id,
+            name,
+            mac,
+            isUp,
+            values.asNetworkInterfaceValues(),
+            speed.asSpeed(),
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.network.NetworkInterfaceSpeed.asSpeed(): NetworkInterfaceSpeed {
     return NetworkInterfaceSpeed(
-        receiveBytesPerSecond, sendBytesPerSecond
+            receiveBytesPerSecond, sendBytesPerSecond
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.network.NetworkInterfaceValues.asNetworkInterfaceValues(): NetworkInterfaceValues {
     return NetworkInterfaceValues(
-        speed, bytesReceived, bytesSent, packetsReceived, packetsSent, inErrors, outErrors
+            speed, bytesReceived, bytesSent, packetsReceived, packetsSent, inErrors, outErrors
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.cpu.CpuLoad.asCpuLoadEntity(id: UUID): CpuLoad {
     return CpuLoad(
-        id,
-        usagePercentage,
-        systemLoadAverage,
-        loadAverages.asLoadAverage(id),
-        coreLoads.map { it.asCoreLoad(id) },
-        cpuHealth.asCpuHealth(id),
-        processCount,
-        threadCount
+            id,
+            usagePercentage,
+            systemLoadAverage,
+            loadAverages.asLoadAverage(id),
+            coreLoads.map { it.asCoreLoad(id) },
+            cpuHealth.asCpuHealth(id),
+            processCount,
+            threadCount
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.cpu.LoadAverages.asLoadAverage(id: UUID): LoadAverages {
     return LoadAverages(
-        id,
-        oneMinute,
-        fiveMinutes,
-        fifteenMinutes
+            id,
+            oneMinute,
+            fiveMinutes,
+            fifteenMinutes
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.cpu.CpuHealth.asCpuHealth(id: UUID): CpuHealth {
     return CpuHealth(
-        id,
-        temperatures,
-        voltage,
-        fanRpm,
-        fanPercent
+            id,
+            temperatures,
+            voltage,
+            fanRpm,
+            fanPercent
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.cpu.CoreLoad.asCoreLoad(id: UUID): CoreLoad {
     return CoreLoad(
-        UUID.randomUUID(),
-        null,
-        id,
-        percentage
+            UUID.randomUUID(),
+            null,
+            id,
+            percentage
     )
 }
 
 fun ContainerMetrics.asEntity(): ContainerStatisticsEntity {
     return ContainerStatisticsEntity(
-        id,
-        Instant.now(),
-        currentPid,
-        cpuUsage.asCpuUsageEntity(),
-        memoryUsage.asMemoryUsageEntity(),
-        networkUsage.asNetworkUsageEntity(),
-        blockIOUsage.asBlockIOUsageEntity()
+            UUID.randomUUID(),
+            id,
+            Instant.now(),
+            currentPid,
+            cpuUsage.asCpuUsageEntity(),
+            memoryUsage.asMemoryUsageEntity(),
+            networkUsage.asNetworkUsageEntity(),
+            blockIOUsage.asBlockIOUsageEntity()
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.docker.BlockIOUsage.asBlockIOUsageEntity(): BlockIOUsage {
     return BlockIOUsage(
-        bytesWritten = bytesWritten,
-        bytesRead = bytesRead
+            bytesWritten = bytesWritten,
+            bytesRead = bytesRead
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.docker.NetworkUsage.asNetworkUsageEntity(): NetworkUsage {
     return NetworkUsage(
-        bytesReceived = bytesReceived,
-        bytesTransferred = bytesTransferred
+            bytesReceived = bytesReceived,
+            bytesTransferred = bytesTransferred
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.docker.MemoryUsage.asMemoryUsageEntity(): MemoryUsage {
     return MemoryUsage(
-        usagePercent = usagePercent,
-        usageBytes = usageBytes,
-        limitBytes = limitBytes
+            usagePercent = usagePercent,
+            usageBytes = usageBytes,
+            limitBytes = limitBytes
     )
 }
 
 private fun com.krillsson.sysapi.core.domain.docker.CpuUsage.asCpuUsageEntity(): CpuUsage {
     return CpuUsage(
-        usagePercentPerCore = usagePercentPerCore,
-        usagePercentTotal = usagePercentTotal,
-        throttlingData = throttlingData.asThrottlingDataEntity()
+            usagePercentPerCore = usagePercentPerCore,
+            usagePercentTotal = usagePercentTotal,
+            throttlingData = throttlingData.asThrottlingDataEntity()
     )
 }
 
