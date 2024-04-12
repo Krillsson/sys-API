b806da4f4d3bb8449c1ee47d3ff70abcdf12babe
Christian <christian.jensen@m.co>
2024-04-12 08:32:04 +0200

    #131 fix containerId type in history lookup

--- src/main/kotlin/com/krillsson/sysapi/core/history/db/ContainerStatisticsEntity.kt
+++ src/main/kotlin/com/krillsson/sysapi/core/history/db/ContainerStatisticsEntity.kt
@@ -25,22 +25,22 @@ class ContainerStatisticsDAO {
         }
     }
 
-    fun findAllBetween(id: String, from: Instant, to: Instant): List<ContainerStatisticsEntity> {
+    fun findAllBetween(containerId: String, from: Instant, to: Instant): List<ContainerStatisticsEntity> {
         val builder = em.criteriaBuilder
         val query: CriteriaQuery<ContainerStatisticsEntity> = builder.createQuery(ContainerStatisticsEntity::class.java)
         val root: Root<ContainerStatisticsEntity> = query.from(ContainerStatisticsEntity::class.java)
         val between = builder
                 .between(root.get("timestamp"), from, to)
-        val equal = builder.equal(root.get<String>("id"), id)
+        val equal = builder.equal(root.get<String>("containerId"), containerId)
         val condition = builder.and(between, equal)
         return em.createQuery(query.where(condition)).resultList
     }
 
-    fun findLatest(id: String): ContainerStatisticsEntity? {
+    fun findLatest(containerId: String): ContainerStatisticsEntity? {
         val cb = em.criteriaBuilder
         val query: CriteriaQuery<ContainerStatisticsEntity> = cb.createQuery(ContainerStatisticsEntity::class.java)
         val root: Root<ContainerStatisticsEntity> = query.from(ContainerStatisticsEntity::class.java)
-        val idPredicate = cb.equal(root.get<String>("id"), id)
+        val idPredicate = cb.equal(root.get<String>("containerId"), containerId)
         query.where(idPredicate)
         query.orderBy(cb.desc(root.get<Instant>("timestamp")))
         val results = em.createQuery(query)
