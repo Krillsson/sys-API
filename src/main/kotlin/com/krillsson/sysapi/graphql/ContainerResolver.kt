7c192366f23239df278709a68c85af727c51611a
Christian <christian.jensen@m.co>
2024-04-14 17:14:46 +0200

    #131 updates to prepare for release

--- src/main/kotlin/com/krillsson/sysapi/graphql/ContainerResolver.kt
+++ src/main/kotlin/com/krillsson/sysapi/graphql/ContainerResolver.kt
@@ -1,14 +1,12 @@
 package com.krillsson.sysapi.graphql
 
-import com.krillsson.sysapi.core.domain.docker.Container
+import com.krillsson.sysapi.core.domain.docker.ContainerMetricsHistoryEntry
 import com.krillsson.sysapi.docker.ContainerManager
-import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 
 @Controller
 class ContainerResolver(val containerManager: ContainerManager) {
-
-    @SchemaMapping(typeName="DockerContainerMetricsHistoryEntry", field="metrics")
-    fun metrics(container: Container) = containerManager.statsForContainer(container.id)
+    @SchemaMapping(typeName = "DockerContainerMetricsHistoryEntry", field = "metrics")
+    fun metrics(container: ContainerMetricsHistoryEntry) = containerManager.statsForContainer(container.containerId)
 }
