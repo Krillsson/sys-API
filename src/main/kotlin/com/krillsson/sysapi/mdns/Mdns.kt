4171d364d01c2595251159de6831570fbecc2c43
Christian <christian.jensen@m.co>
2024-04-30 06:58:29 +0200

    #140 register mDNS async to improve startup performance

--- src/main/kotlin/com/krillsson/sysapi/mdns/Mdns.kt
+++ src/main/kotlin/com/krillsson/sysapi/mdns/Mdns.kt
@@ -9,14 +9,19 @@ import jakarta.annotation.PreDestroy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.core.env.getProperty
+import org.springframework.core.task.TaskExecutor
+import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.stereotype.Service
 import javax.jmdns.JmDNS
 import javax.jmdns.ServiceInfo
+import kotlin.time.measureTime
 
 @Service
 class Mdns(
     private val configuration: YAMLConfigFile,
-    private val connectivityCheckService: ConnectivityCheckService
+    private val connectivityCheckService: ConnectivityCheckService,
+    private val threadPoolTaskExecutor: ThreadPoolTaskExecutor
 ) {
 
     private val logger by logger()
@@ -29,8 +34,14 @@ class Mdns(
     @PostConstruct
     fun start() {
         if (configuration.mDNS.enabled) {
-            val address = connectivityCheckService.findLocalIp()
-            jmdns = JmDNS.create(address)
+            register()
+        }
+    }
+
+    fun register() {
+        val address = connectivityCheckService.findLocalIp()
+        jmdns = JmDNS.create(address)
+        threadPoolTaskExecutor.execute {
             listOf(
                 "http" to env.getProperty<Int>("http.port", 8080),
                 "https" to env.getProperty<Int>("server.port", 8443),
@@ -40,9 +51,11 @@ class Mdns(
                 .forEach { (scheme, port) ->
                     val serviceType = "_$scheme._tcp.local"
                     val serviceName = "${EnvironmentUtils.hostName} - sys-API $scheme"
-                    logger.info("Registering mDNS: $serviceType with name: $serviceName at port $port")
-                    val serviceInfo = ServiceInfo.create(serviceType, serviceName, port, "GraphQL at /graphql")
-                    jmdns.registerService(serviceInfo)
+                    val result = measureTime {
+                        val serviceInfo = ServiceInfo.create(serviceType, serviceName, port, "GraphQL at /graphql")
+                        jmdns.registerService(serviceInfo)
+                    }
+                    logger.info("Registered mDNS: $serviceType with name: $serviceName at port $port (took ${result.inWholeMilliseconds}ms)")
                 }
         }
     }
