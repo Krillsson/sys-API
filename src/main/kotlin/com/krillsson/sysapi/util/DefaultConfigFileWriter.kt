c80b4dfbb7d9511153407b8b831fec0d326c259a
Christian <christian.jensen@m.co>
2025-02-14 13:29:58 +0100

    #182 cleanup code

--- src/main/kotlin/com/krillsson/sysapi/util/DefaultConfigFileWriter.kt
+++ src/main/kotlin/com/krillsson/sysapi/util/DefaultConfigFileWriter.kt
@@ -6,46 +6,43 @@ import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 import java.io.File
-import java.io.InputStream
 
 @Component
 class DefaultConfigFileWriter : ApplicationRunner {
 
     val logger by logger()
 
+    companion object {
+        private val USER_CONFIG_FILE = File(FileSystem.config, USER_CONFIG)
+        private val SPRING_CONFIG_FILE = File(FileSystem.config, SPRING_CONFIG)
+        private const val DEFAULT_USER_CONFIG_PATH = "/config/${USER_CONFIG}"
+        private const val DEFAULT_SPRING_CONFIG_PATH = "/config/${SPRING_CONFIG}"
+    }
+
 
     @Throws(Exception::class)
     override fun run(args: ApplicationArguments) {
         FileSystem.assertConfigDirectory()
-        val userConfig = File(FileSystem.config, USER_CONFIG)
-        val springConfig = File(FileSystem.config, SPRING_CONFIG)
-
-        if (!userConfig.exists()) {
-            val fallbackUserConfigFile = javaClass.getResourceAsStream("/config/$USER_CONFIG")
-            writeFile(fallbackUserConfigFile, userConfig)
-        }
-
-        if (!springConfig.exists()) {
-            val fallbackSpringConfigFile = javaClass.getResourceAsStream("/config/$SPRING_CONFIG")
-            writeFile(fallbackSpringConfigFile, springConfig)
-        }
-
+        writeDefaultFile(USER_CONFIG_FILE, DEFAULT_USER_CONFIG_PATH)
+        writeDefaultFile(SPRING_CONFIG_FILE, DEFAULT_SPRING_CONFIG_PATH)
     }
 
-    private fun writeFile(fileStream: InputStream?, destinationFile: File) {
-        if (fileStream == null) {
-            logger.warn("Resource not found, skipping: $destinationFile")
-            return
-        }
+    private fun writeDefaultFile(destinationFile: File, defaultUserConfigPath: String) {
+        if (!destinationFile.exists()) {
+            val fileStream = javaClass.getResourceAsStream(defaultUserConfigPath)
+            if (fileStream == null) {
+                logger.warn("Resource not found, skipping: $destinationFile")
+                return
+            }
 
-        logger.info("Writing default config to $destinationFile")
+            logger.info("Writing default config to $destinationFile")
 
-        destinationFile.outputStream().use { output ->
-            fileStream.use { input ->
-                input.copyTo(output)
+            destinationFile.outputStream().use { output ->
+                fileStream.use { input ->
+                    input.copyTo(output)
+                }
             }
         }
     }
-
 
 }
